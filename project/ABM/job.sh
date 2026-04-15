46641f7a1e4fd5ad09f7ed45d0671c8fd43706cc
Angus-Eastell <sd23327@bristol.ac.uk>
2026-04-15 22:34:35 +0100

    efficiency

--- project/ABM/job.sh
+++ project/ABM/job.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=coms038604
-#SBATCH --partition=cpu
-#SBATCH --time=02:00:00
-#SBATCH --cpus-per-task=8
+#SBATCH --job-name=testing_experiments
+#SBATCH --account=coms038604
+#SBATCH --partition=compute
+#SBATCH --time=8:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=16G
 #SBATCH --output=slurm-%x_%j.out
+#SBATCH --error=slurm-%x_%j.out
 
 set -eu
 
-# Load conda
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate base   # or abm_env if you cloned it
+# Load Python
+module load languages/python/3.12.3
 
 # Go to your ABM folder
-cd /user/home/sd23327/ABM
+cd "$SLURM_SUBMIT_DIR"
 
 # Run your model
-python experiments.py
+python run.py
