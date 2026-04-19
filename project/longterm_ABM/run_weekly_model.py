fa4e303f7b50c1023f4143420a2c334999ee94d4
3llag0dek <159832151+3llag0dek@users.noreply.github.com>
2026-04-19 16:20:04 +0100

    new updates and analysis

--- project/longterm_ABM/run_weekly_model.py
+++ project/longterm_ABM/run_weekly_model.py
@@ -1,30 +1,34 @@
 import pandas as pd
 from WeeklyModel_ import WeeklySpeciesModel
 
-all_results = []
+wolf_numbers = [20, 40, 45, 50]
 
-for run_id in range(2):
-    model = WeeklySpeciesModel(max_steps=50000)
 
-    while model.running:
-        model.step()
+for wolf_n in wolf_numbers:
+    all_results = []
 
-    results = model.datacollector.get_model_vars_dataframe().copy()
-    results["Week"] = results.index * 0.5
-    results["Year"] = results["Week"] / 52
-    results["Run"] = run_id
+    for run_id in range(20):
+        model = WeeklySpeciesModel(max_steps=50000, init_total_wolves=wolf_n)
 
-    
-    results["Extinction"] = model.stop_reason == "extinction"
+        while model.running:
+            model.step()
 
-    all_results.append(results)
+        results = model.datacollector.get_model_vars_dataframe().copy()
+        results["Week"] = results.index * 0.5
+        results["Year"] = results["Week"] / 52
+        results["Run"] = run_id
 
-    if model.stop_reason == "extinction":
-        print(f"Model {run_id} went extinct at step {model.extinction_step}. Saved with extinction label.")
-    else:
-        print(f"Model {run_id} finished at max steps and was saved.")
+        
+        results["Extinction"] = model.stop_reason == "extinction"
 
-combined = pd.concat(all_results, ignore_index=True)
-combined.to_csv("weekly_model_multiple_runs.csv", index=False)
+        all_results.append(results)
 
-print("Saved results to weekly_model_multiple_runs.csv")
+        if model.stop_reason == "extinction":
+            print(f"Model {run_id} went extinct at step {model.extinction_step}. Saved with extinction label.")
+        else:
+            print(f"Model {run_id} finished at max steps and was saved.")
+
+    combined = pd.concat(all_results, ignore_index=True)
+    combined.to_csv(f"Model_wolf{wolf_n}_20runs.csv", index=False)
+
+    print(f"Model_wolf{wolf_n}_20runs.csv")
